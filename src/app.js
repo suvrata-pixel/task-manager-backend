449f75db49c7e2893decfe23250262362246f5ab
suvrata-pixel <suvrataga4@gmail.com>
2026-09-25 15:43:40 +0530

    small modification in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+  const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
